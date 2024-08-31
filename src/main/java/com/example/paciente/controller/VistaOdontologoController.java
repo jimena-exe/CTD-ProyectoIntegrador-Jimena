1066cb4a9a2ea1c3c8403b0f632881220409cd50
jimena-exe <jimena.alvarezva@amigo.edu.co>
2024-08-31 17:45:40 -0500

    vista odontologo

--- src/main/java/com/example/paciente/controller/VistaOdontologoController.java
+++ src/main/java/com/example/paciente/controller/VistaOdontologoController.java
@@ -1,11 +1,15 @@
 package com.example.paciente.controller;
 
 import com.example.paciente.entity.Odontologo;
+import com.example.paciente.entity.Turno;
 import com.example.paciente.service.impl.OdontologoService;
+import com.example.paciente.service.impl.TurnoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Set;
 
 @Controller
 public class VistaOdontologoController {
@@ -16,6 +20,7 @@ public class VistaOdontologoController {
         this.odontologoService = odontologoServive;
     }
 
+    /*
     @GetMapping("/index")
     public String bienvenidaOdontologo(Model model, @RequestParam Integer id){
         Odontologo odontologo = odontologoService.buscarPorId(id).get();
@@ -23,5 +28,15 @@ public class VistaOdontologoController {
         model.addAttribute("apellido",odontologo.getApellido());
         return "vista/odontologo";
     }
+     */
+
+    //prueba
+    @GetMapping("/index")
+    public String bienvenidaOdontologo(Model model){
+        model.addAttribute("nombre","Amanda");
+        model.addAttribute("apellido","Torres");
+        return "/vista/odontologo";
+    }
+
 
 }
